df9324f469a596a13e1a3440378571b78e1ac01c
Hafizh Herdi <hherdinaufal@gmail.com>
2017-06-20 18:17:32 +0700

    firebaseDB firsttry

---
+++ app/src/main/java/id/web/twoh/twohfirebase/FirebaseDBActivity.java
@@ -0,0 +1,8 @@
+package id.web.twoh.twohfirebase;
+
+/**
+ * Created by Herdi_WORK on 07.05.17.
+ */
+
+public class FirebaseDBActivity {
+}
---
+++ app/src/main/java/id/web/twoh/twohfirebase/model/Barang.java
@@ -0,0 +1,8 @@
+package id.web.twoh.twohfirebase;
+
+/**
+ * Created by Herdi_WORK on 18.06.17.
+ */
+
+public class model {
+}
