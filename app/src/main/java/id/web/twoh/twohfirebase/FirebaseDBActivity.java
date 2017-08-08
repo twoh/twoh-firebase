ae596509a5933914d78ddba1368b9c5e51b540cf
Hafizh Herdi <hherdinaufal@gmail.com>
2017-08-08 18:27:14 +0700

    connecting to firebase

--- app/src/main/java/id/web/twoh/twohfirebase/FirebaseDBActivity.java
+++ app/src/main/java/id/web/twoh/twohfirebase/FirebaseDBActivity.java
@@ -3,9 +3,14 @@ package id.web.twoh.twohfirebase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import id.web.twoh.twohfirebase.model.Barang;
 
 /**
  * Created by Herdi_WORK on 07.05.17.
@@ -13,12 +18,23 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseDBActivity extends AppCompatActivity {
 
-    private DatabaseReference database;
+    private Button btCreateDB;
+    private Button btViewDB;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crud);
-        database = FirebaseDatabase.getInstance().getReference();
+
+        btCreateDB = (Button) findViewById(R.id.bt_createdata);
+        btViewDB = (Button) findViewById(R.id.bt_viewdata);
+
+        btCreateDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(FirebaseDBCreateActivity.getActIntent(FirebaseDBActivity.this));
+            }
+        });
     }
+
 }
--- app/src/main/java/id/web/twoh/twohfirebase/FirebaseDBCreateActivity.java
+++ app/src/main/java/id/web/twoh/twohfirebase/FirebaseDBCreateActivity.java
@@ -1,8 +1,54 @@
 package id.web.twoh.twohfirebase;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import id.web.twoh.twohfirebase.model.Barang;
+
 /**
  * Created by Herdi_WORK on 08.08.17.
  */
 
-public class FirebaseDBCreateActivity {
+public class FirebaseDBCreateActivity extends AppCompatActivity {
+
+    private DatabaseReference database;
+    private Button btSubmit;
+    private EditText etNama;
+    private EditText etMerk;
+    private EditText etHarga;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db_create);
+        etNama = (EditText) findViewById(R.id.et_namabarang);
+        etMerk = (EditText) findViewById(R.id.et_merkbarang);
+        etHarga = (EditText) findViewById(R.id.et_hargabarang);
+        btSubmit = (Button) findViewById(R.id.bt_submit);
+        database = FirebaseDatabase.getInstance().getReference();
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitBarang(new Barang(etNama.getText().toString(),etMerk.getText().toString(),etHarga.getText().toString()));
+            }
+        });
+    }
+
+    private void submitBarang(Barang barang){
+        database.child("barang").push().setValue(barang);
+    }
+
+    public static Intent getActIntent(Activity activity){
+        return new Intent(activity, FirebaseDBCreateActivity.class);
+    }
 }
