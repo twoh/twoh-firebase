1675ff762f5cdb14809dd3b78f537008500872d3
Hafizh Herdi <hherdinaufal@gmail.com>
2017-10-03 13:29:40 +0700

    new perfmon library

--- modulofirebaso/src/androidTest/java/vending/tokopedia/web/id/modulofirebaso/ExampleInstrumentedTest.java
+++ modulofirebaso/src/androidTest/java/vending/tokopedia/web/id/modulofirebaso/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("vending.tokopedia.web.id.modulofirebaso", appContext.getPackageName());
+        assertEquals("vending.tokopedia.web.id.modulofirebaso.test", appContext.getPackageName());
     }
 }
